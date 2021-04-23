9f81c994d3e3fbb02130a08fbb6a4f067b0e2268
juanmoo <juanmoo@me.com>
2021-04-23 18:36:36 -0400

    Basic API with synthetic data

---
+++ README.md
@@ -0,0 +1,8 @@
+# API for polymer reaction synthesis extractor
+API Deployement of reaction synthesis extraction model.
+
+## Installation
+```bash
+pip install -r requirements.txt
+```
+
---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from dotenv import load_dotenv
+from os import environ
+from pathlib import Path
+from random import shuffle
+import json
+import os
+from time import sleep
+
+# App Setup
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+class Extractor(Resource):
+    def post(self):
+
+        if request.is_json:
+            data = request.json
+        else:
+            return {
+                'error': "Please format paragraphs to be extracted in a JSON object."
+            }
+        
+        paragraphs = data.get('paragraphs', [])
+
+        # TEMP: Use synthetic data for now
+        synthetic_data_dir = environ.get('SYNTHETIC_DATA_DIR')
+        synthetic_extractions_path = os.path.join(synthetic_data_dir, 'extractions.json')
+        extractions = []
+        with open(synthetic_extractions_path, 'r') as f:
+            extractions = json.load(f)
+
+        idxs = list(range(len(extractions)))
+        shuffle(idxs)
+        idxs = idxs[:len(paragraphs)]
+
+        sampled_extractions = [extractions[j] for j in idxs]
+
+        return {
+            'extractions': sampled_extractions
+        }
+
+api.add_resource(Extractor, '/extract')
+
+
+
+if __name__ == '__main__':
+
+    print('Starting up server ...')
+
+    # Load configuration file
+    config_path = os.path.join(os.path.realpath('.'), '.env')
+    
+    load_dotenv(dotenv_path=config_path)
+    port=environ.get('PORT')
+
+    app.run(debug=True, host='0.0.0.0', port=port)
