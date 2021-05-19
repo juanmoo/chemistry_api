da2122a6ebf1960a004ed5eb6ceb0f91f49d89fa
Juan M Ortiz <juanmoo@me.com>
2021-05-19 10:01:06 -0400

    extract skeleton

---
+++ extract.py
@@ -0,0 +1,14 @@
+'''
+Extraction Module
+'''
+
+def create_bio(paragraphs):
+    pass
+
+def extract_paragraphs(paragraph_list: list) -> list:
+
+    # tokenize text on paragraphs
+    tokenized_list = []
+
+    return extractions
+
